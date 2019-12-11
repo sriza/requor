85eaa9eb3ef74c213a352327eb4aed4d4a84ec84
sriza <sriza054@mail.com>
2019-12-11 22:50:46 +0545

    created CategoryModel and rendered popular questions

--- qna_app/urls.py
+++ qna_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import addquestion
+from .views import addquestion,popular
 
 urlpatterns = [
-    path('addquestion/',addquestion,name="addquestion")  
+    path('addquestion/',addquestion,name="addquestion"), 
+    path('popular/',popular,name="popularquestions") 
 ]
