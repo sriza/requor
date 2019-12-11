6ec818b2ea26514f7e1d79cd402a652cb7dc2daf
sriza <sriza054@mail.com>
2019-12-11 08:52:11 +0545

    models created for questions and answers

--- qna_app/models.py
+++ qna_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class QuestionModel(models.Model):
+    title=models.CharField(max_length=255)
+    posted_by=models.CharField(max_length=120)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    question_desc=models.TextField()
+    question_img=models.ImageField(upload_to="QuestionImg")
+
+
+class AnswerModel(models.Model):
+    ans_by=models.CharField(max_length=120)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    votes=models.IntegerField()
+    is_accepted=models.BooleanField()
+    answer_desc=models.TextField()
+    answer_img=models.ImageField(upload_to="AnswerImg")
+    question=models.ForeignKey(QuestionModel,on_delete=models.CASCADE)
+
+
+
+
---
+++ user_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from user_app.views import 
+
+urlpatterns = [
+    path('ana/',),
+]
